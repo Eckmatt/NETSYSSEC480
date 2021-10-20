8cba1e5c0d06f47f74b88439fa930853e5bcbcae
ECKMATT <meckhardt14@yahoo.com>
2021-10-20 18:25:31 -0400

    Updated script for Milestone 6

--- 480-utils/480-utils.ps1
+++ 480-utils/480-utils.ps1
@@ -1,66 +1,97 @@
-Try
-{
-    $vserver= Read-Host "Please enter the FQDN or IP address of your Vcenter server: " 
-    Connect-VIServer($vserver) -ErrorAction Stop
+Function connect_server(){
+    Try
+    {
+        $vserver= Read-Host "Please enter the FQDN or IP address of your Vcenter server: " 
+        Connect-VIServer($vserver) -ErrorAction Stop
 
-}Catch{
-    Write-Output "!!!Error!!! - Could not connect to vCenter. Check your Vcenter server name and your Vcenter credentials."
-    
-}
-Try{
-    $vmName = Read-Host "Enter the VM Name you wish to clone: "
-    $vm=Get-VM -Name $vmName -ErrorAction Stop
-
-}Catch{
-    Write-Output "!!!Error!!! - Enter an existing VM name"
-    exit
-}
-Try{
-    $snapName = Read-Host "Enter the Name of "$vmName"'s Snapshot you wish to clone" 
-    $snapshot=Get-Snapshot -VM $vm -Name $snapName -ErrorAction Stop
-}Catch{
-    Write-Output "!!!Error!!! - Snapshot not found!"
-    exit
-}Try{
-    $dname = Read-Host "Enter the Name of the datastore you wish to store your clone: "
-    $dstore = Get-DataStore $dname -ErrorAction Stop
-    
-
-}Catch{
-    Write-Warning $Error[0]
-    exit
-
-}Try{
-    $hostname = Read-Host "Enter the Name of the esxi host you wish to store your clone: "
-    $vmhost = Get-VMHost -Name $hostname -ErrorAction Stop
-
-}Catch{
-    Write-Warning $Error[0]
-    exit
-}
+    }Catch{
+        Write-Output "!!!Error!!! - Could not connect to vCenter. Check your Vcenter server name and your Vcenter credentials."
         
-$choice = Read-Host "Create a Linked Clone or Full Clone? Enter [L/l] for Linked Clone or [F/f] for Full Clone"
-If($choice -eq 'F' -or $choice -eq 'f' )
-{
-    $Tempname = "{0}.temp" -f $vmName
-    $Tempvm = New-VM -Name $Tempname -VM $vm -LinkedClone -ReferenceSnapshot $snapshot -VMHost $vmhost -Datastore $dstore
-    $newname = Read-Host "Enter a name for your New VM"
+    }
+}
 
-    $newvm = New-VM -Name $newname -VM $Tempvm -VMHost $vmhost -Datastore $dstore 
-    $newvm | new-snapshot -Name "Base"
-    $Tempvm | Remove-VM
+Function select_vm(){
+    Try{
+        $vmName = Read-Host "Enter the VM Name you wish to clone: "
+        $vm=Get-VM -Name $vmName -ErrorAction Stop
+        return $vm
 
-}elseif ($choice -eq 'L' -or $choice -eq 'l' ) {
+    }Catch{
+        Write-Output "!!!Error!!! - Enter an existing VM name"
+        exit
+    }
+}
 
-    $newname = "{0}.linked" -f $vmName
-    $newvm = New-VM -Name $newname -VM $vm -LinkedClone -ReferenceSnapshot $snapshot -VMHost $vmhost -Datastore $dstore
+Function pick_snapshot(){
+    [CmdletBinding()]
+    param (
+        $vm
+    )
+    Try{
+        $snapName = Read-Host "Enter the Name of "$vm.Name"'s Snapshot you wish to clone" 
+        $snapshot=Get-Snapshot -VM $vm -Name $snapName -ErrorAction Stop
+        return $snapshot
+    }Catch{
+        Write-Output "!!!Error!!! - Snapshot not found!"
+        exit
+}
+}
+Function pick_datastore(){
+    Try{
+        $dname = Read-Host "Enter the Name of the datastore you wish to store your clone"
+        $dstore = Get-DataStore $dname -ErrorAction Stop
+        return $dstore
+    
 
-}else{
-    throw "Select Either [L]inked Clone or [F]ull Clone"
+    }Catch{
+        Write-Warning $Error[0]
+        exit
+
+    }
+}
+Function pick_hostname(){
+    Try{
+        $hostname = Read-Host "Enter the Name of the esxi host you wish to store your clone"
+        $vmhost = Get-VMHost -Name $hostname -ErrorAction Stop
+        return $vmhost
+
+    }Catch{
+        Write-Warning $Error[0]
+        exit
+    }
+
 }
 
 
+function cloner () {
+    connect_server
+    $vm = select_vm
+    $snapshot = pick_snapshot -vm $vm
+    $vmhost = pick_hostname
+    $dstore = pick_datastore
 
+    $choice = Read-Host "Create a Linked Clone or Full Clone? Enter [L/l] for Linked Clone or [F/f] for Full Clone"
+    If($choice -eq 'F' -or $choice -eq 'f' )
+    {
+        $Tempname = "{0}.temp" -f $vm.Name
+        $Tempvm = New-VM -Name $Tempname -VM $vm -LinkedClone -ReferenceSnapshot $snapshot -VMHost $vmhost -Datastore $dstore
+        $newname = Read-Host "Enter a name for your New VM"
+
+        $newvm = New-VM -Name $newname -VM $Tempvm -VMHost $vmhost -Datastore $dstore 
+        $newvm | new-snapshot -Name "Base"
+        $Tempvm | Remove-VM
+
+    }elseif ($choice -eq 'L' -or $choice -eq 'l' ) {
+
+        $newname = "{0}.linked" -f $vm.Name
+        $newvm = New-VM -Name $newname -VM $vm -LinkedClone -ReferenceSnapshot $snapshot -VMHost $vmhost -Datastore $dstore
+
+    }else{
+        throw "Select Either [L]inked Clone or [F]ull Clone"
+    }
+
+}
+cloner
 
 
 #Try{}Catch{}
